e656b0e8dfa1d612651aa79b76a46bf3889d8830
sathwik kuncham <sathwikkuncham@gmail.com>
2020-05-25 09:57:04 +0000

    SQL added. Cool.

---
+++ Pages/Restaurants/Details.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+        [TempData]
+        public string Message { get; set; }
+        public DetailsModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetRestaurantbyId(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+        public IActionResult OnGet(int? restaurantId)
+        {
+            if(restaurantId.HasValue)
+                Restaurant = restaurantData.GetRestaurantbyId(restaurantId.Value);
+            else
+            {
+                Restaurant = new Restaurant();
+            }
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+         
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                return Page();
+            }
+            if (Restaurant.Id > 0)
+                Restaurant = restaurantData.Update(Restaurant);
+            else
+                Restaurant = restaurantData.Add(Restaurant);
+            restaurantData.commit();
+            TempData["Message"] = "Restaurant Created";
+            return RedirectToPage("./Details", new { restaurantId = Restaurant.Id });
+
+        }
+    }
+}
